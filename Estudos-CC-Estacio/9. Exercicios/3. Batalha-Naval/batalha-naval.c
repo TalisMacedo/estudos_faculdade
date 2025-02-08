26555db9e6d05c86dcabcc891c2e9d00e62eef4d
talismacedo <talismacedo@yahoo.com>
2025-02-07 22:13:22 -0300

    att: numeros aletorios

--- Estudos-CC-Estacio/9. Exercicios/3. Batalha-Naval/batalha-naval.c
+++ Estudos-CC-Estacio/9. Exercicios/3. Batalha-Naval/batalha-naval.c
@@ -1,59 +1,86 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-#define LINHA 3
-#define COLUNA 3
+#define LINHA 10
+#define COLUNA 10
 
 int main()
 {
   int matriz[LINHA][COLUNA];
-  int navio1 = 3;
-  int navio2 = 4;
-  int navio3 = 5;
-  int posicao = 1;
-  int linha, coluna;
+  int navioA, navioB;
+  int tabuleiro = 1;
+  int coordenadaX, coordenadaY;
+
+  // Inicializa o gerador de números aleatórios
+  srand(time(NULL));
+
+  // Gera posições aleatórias para os navios
+  navioA = rand() % 100 + 1;
+  do {
+      navioB = rand() % 100 + 1;
+  } while (navioB == navioA); // Garante que navios não estejam na mesma posição
 
 
-  for(int i = 0; i < LINHA; i++)
-  {
-    for(int j = 0; j < COLUNA; j++)
-    {
-      matriz[i][j] = posicao;
-      posicao++;
+  // Inicializa o tabuleiro
+  for(int i = 0; i < LINHA; i++) {
+    for(int j = 0; j < COLUNA; j++) {
+      matriz[i][j] = tabuleiro;
+      tabuleiro++;
     }
-    printf("\n");
   }
 
-  printf("Esse é o campo de batlha: \n");
-  for(int i = 0; i < LINHA; i++)
-  {
-    for(int j = 0; j < COLUNA; j++)
-    {
-      printf("[%d] ", matriz[i][j]);
-    }
+  while (1) {
+  
     printf("\n");
-  }
-
-  printf("Qual linha: ");
-  scanf("%d", &linha);
-  printf("Qual coluna: ");
-  scanf("%d", &coluna);
-  linha -= 1;
-  coluna -= 1;
-
-  for(int i = 0; i < LINHA; i++)
-  {
-    for(int j = 0; j < COLUNA; j++)
-    {
-      if(linha != navio1 && coluna != navio1)
-      {
-        matriz[linha][coluna] = 0;
+    printf(" -> Esse é o campo de batalha: \n");
+    printf("\n");
+    for(int i = 0; i < LINHA; i++) {
+      for(int j = 0; j < COLUNA; j++) {
+        printf("[%2d] ", matriz[i][j]);
       }
-      printf("[%d] ", matriz[i][j]);
+      printf("\n");
     }
     printf("\n");
+
+    printf(" -> Digite a Coordenada X: ");
+    scanf("%d", &coordenadaX);
+    printf(" -> Digite a Coordenada Y: ");
+    scanf("%d", &coordenadaY);
+
+    // Verifica se as coordenadas são válidas
+    if (coordenadaX < 0 || coordenadaX >= LINHA + 1 || coordenadaY < 0 || coordenadaY >= COLUNA + 1) {
+      printf("Coordenada inválida! Digite valores entre 1 e 10.\n");
+      continue;
+    }
+
+    coordenadaX -= 1;
+    coordenadaY -= 1;
+
+    if (matriz[coordenadaX][coordenadaY] == navioA) {
+      matriz[coordenadaX][coordenadaY] = 3;
+      printf("\033[0;32m -> Parabéns!! Você afundou o navioA nas COORDENADAS [%d , %d]\033[0m\n", coordenadaX + 1, coordenadaY + 1);
+      printf("\n");
+      break;
+    } else if (matriz[coordenadaX][coordenadaY] == navioB) {
+      matriz[coordenadaX][coordenadaY] = 3;
+      printf("\033[0;32m -> Parabéns!! Você afundou o navioB nas COORDENADAS [%d , %d]\033[0m\n", coordenadaX + 1, coordenadaY + 1);
+      printf("\n");
+      break;
+    } else {
+      matriz[coordenadaX][coordenadaY] = 0;
+      printf(" \033[0;31m-> ERROU! Tente novamente.\033[0m\n");
+      printf("\n");
+    }
   }
 
-
+   printf("\nTabuleiro final:\n");
+   for(int i = 0; i < LINHA; i++) {
+     for(int j = 0; j < COLUNA; j++) {
+       printf("[%2d] ", matriz[i][j]);
+     }
+     printf("\n");
+   }
 
   return 0;
 }
